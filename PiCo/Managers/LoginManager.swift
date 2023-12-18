09eb633551dc5028344142aca30dd9460952d0b0
JDeoks <duginee@naver.com>
2023-12-18 20:42:22 +0900

    Feat: LoginManager - handleFirebaseAuthError 추가 인터넷 연결이슈는 로그아웃 하지 않고 alert 띄우도록 수정

--- PiCo/Managers/LoginManager.swift
+++ PiCo/Managers/LoginManager.swift
@@ -118,23 +118,55 @@ class LoginManager {
         }
     }
     
-    /// 서버에서 유저 정보 받아 갱신. 유효하지 않으면 리스너가 rootVC 온보딩으로 바꿈
+    /// 서버에서 유저 정보 받아 갱신
     func fetchAccount() {
         print("LoginManager - fetchAccount()")
 
         if let user = Auth.auth().currentUser {
             // 서버에서 사용자 상태 갱신
             user.reload { error in
-                if let error = error {
-                    print("사용자 상태 갱신 실패: \(error.localizedDescription)")
-                    self.fetchAccountFailed.onNext(())
-                    return
-                } else {
+                guard let error = error else {
                     print("user.reload 성공")
+                    return
                 }
+                self.handleFirebaseAuthError(error: error as NSError)
             }
         } else {
             print("currentUser 없음")
+            self.fetchAccountFailed.onNext(())
+        }
+    }
+    
+    func handleFirebaseAuthError(error: NSError) {
+        switch error.code {
+        case AuthErrorCode.networkError.rawValue:
+            // 네트워크 오류 처리
+            print("네트워크 오류 발생. 인터넷 연결 확인 필요.")
+            //TODO: 네트워크 불량 Alert
+        case AuthErrorCode.userNotFound.rawValue:
+            // 사용자를 찾을 수 없음
+            print("사용자 계정을 찾을 수 없습니다.")
+            self.fetchAccountFailed.onNext(())
+        case AuthErrorCode.userTokenExpired.rawValue:
+            // 사용자 토큰 만료
+            print("사용자 토큰이 만료되었습니다. 다시 로그인해주세요.")
+        case AuthErrorCode.tooManyRequests.rawValue:
+            // 너무 많은 요청
+            print("요청이 너무 많습니다. 잠시 후에 다시 시도해주세요.")
+        case AuthErrorCode.invalidEmail.rawValue, AuthErrorCode.wrongPassword.rawValue:
+            // 잘못된 이메일 또는 비밀번호
+            print("이메일 주소가 잘못되었거나 비밀번호가 틀렸습니다.")
+        case AuthErrorCode.userDisabled.rawValue:
+            // 사용자 계정 비활성화
+            print("사용자 계정이 비활성화되었습니다.")
+            self.fetchAccountFailed.onNext(())
+        case AuthErrorCode.operationNotAllowed.rawValue:
+            // 허용되지 않은 작업
+            print("이 작업은 현재 허용되지 않습니다.")
+            self.fetchAccountFailed.onNext(())
+        default:
+            // 기타 오류
+            print("알 수 없는 오류 발생: \(error.localizedDescription)")
             self.fetchAccountFailed.onNext(())
         }
     }
