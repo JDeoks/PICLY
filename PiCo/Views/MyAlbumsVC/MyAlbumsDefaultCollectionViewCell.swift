a4bc3ee85550b4f8f29e3bf4801cac78c29f5f5b
JDeoks <duginee@naver.com>
2024-01-09 16:05:14 +0900

    Feat: MyAlbumsDefaultCollectionViewCell 추가

---
+++ PiCo/Views/MyAlbumsVC/MyAlbumsDefaultCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyAlbumsDefaultCollectionViewCell.swift
+//  PiCo
+//
+//  Created by JDeoks on 1/9/24.
+//
+
+import UIKit
+
+class MyAlbumsDefaultCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
