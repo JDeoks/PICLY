a3d9f6492fcd71ef5ee873cf753215f8c6856fcc
JDeoks <duginee@naver.com>
2024-02-23 03:23:15 +0900

    Build 1

--- PICLY/PICLYConstants.swift
+++ PICLY/PICLYConstants.swift
@@ -11,8 +11,8 @@ class PICLYConstants {
     
     static let defaultRootURL = URL(string: "https://picly.app/")!
     
-    /// PICLY, 새로운 시작 기본 이미지
-    static let defaultImageURLStr = "https://firebasestorage.googleapis.com/v0/b/pico-a81e4.appspot.com/o/defaultImage.jpg?alt=media&token=7ca520a7-1b08-4196-beca-94d5560f3f1b"
+    /// PICLY, 새로운 시작 기본 이미지. url
+    static let defaultImageURLStr = "https://firebasestorage.googleapis.com/v0/b/picly-d39a2.appspot.com/o/defaultImage.png?alt=media&token=275f47b8-6432-48e0-9e29-830bb631789e"
     
     static let shareExDevBundleID = "com.Deok.PiCo.dev.PiCoShareExtension"
     static let shareExProductionBundleID = "com.Deok.PiCo.PiCoShareExtension"
